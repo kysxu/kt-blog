35d8b99a6502dec6a49e16302e40283efd08e188
KT <felikzz98@gmail.com>
2026-06-23 20:34:39 +0700

    feat: add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/HeroSection";
+
 function App() {
   return (
-    <section>
-      <h1>My First React App on Vite</h1>
-    </section>
-  )
+    <>
+      <HeroSection />
+    </>
+  );
 }
 
-export default App
+export default App;
